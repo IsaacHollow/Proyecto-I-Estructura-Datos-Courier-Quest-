77e9b01f7cde1f5e1e2b20b7a63143dc45e24f7f
GabeQV <gabeqv16@gmail.com>
2025-09-27 17:09:52 -0600

    Actualizacion de pedidos.py

--- src/pedidos.py
+++ src/pedidos.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Tuple
 
 @dataclass
 class Pedido:
-    id: str = ""
-    priority: int = 0
-    payout: float = 0.0
-    weight: float = 0.0
-    deadline: str = ""
-    release_time: int = 0
-    pickup: List[int] = None
-    dropoff: List[int] = None
+    id: str
+    priority: int
+    payout: float
+    weight: float
+    deadline: str
+    release_time: int
+    pickup: Tuple[int, int]
+    dropoff: Tuple[int, int]
